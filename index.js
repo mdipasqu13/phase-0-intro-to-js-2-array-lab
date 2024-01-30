07422d945dee09fcb22cb2a449c809b1e6383b38
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-01-30 14:32:03 -0500

    completed arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+  ];
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  function appendCat(name) {
+    const copyOfCats = cats.slice(0,3);
+    copyOfCats.push(name);
+    return copyOfCats;
+  }
+  function prependCat(name) {
+    const copyOfCats = cats.slice(0,3);
+    copyOfCats.unshift(name);
+    return copyOfCats;
+  }
+  function removeLastCat() {
+    const copyOfCats = cats.slice(0,2);
+    return copyOfCats;
+  }
+  function removeFirstCat() {
+    const copyOfCats = cats.slice(1,3)
+    return copyOfCats;
+  }
